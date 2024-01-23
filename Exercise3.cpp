7a2f004b220b7c92b439af56ee4ee31011aa44ad
Joshua Bricker <joshbricker77@gmail.com>
2024-01-22 21:35:36 -0500

    C++ Number Averager
    This Program finds the average of given numbers
    #include -- preprocessor directive for including contents of a file --- specifically <iostream>  which is essential for input and output operations
    using -- for introducing namespace into the program -- namespaces are used in c++ to organize code into logical groups and prevent name collisions.
    namespace works together with using for declaring a scope 'std' is the stand namespace
    int -- reserved word to declare a variable type -- short for integer
    return -- ends the function and optionally returns a value to the calling function. in the case of 'main' returning 0 typically indicating the program has ended successfully.
    std::cout -- a predefined object in c++ that represents standard output stream. used with the insertion operator << to print data to the output.
    std::endl -- represents end of a line moves cursor to next line and flushes output buffer
    main() -- every c++ program must havea  main function its the entry point where program execution will begin.
    << -- insertion operator used  with std::cout to output the data to standard output(the screen)
    // -- indicates a comment comments are ignored by the compiler used for taking notes and explaining the code for humans
    double -- data type similar to int but used for floating-point numbers(number with decimals)
    = -- assignment operator used to assign value to a variable
    ; -- used to terminate statements like a period for a sentence
    also utilized arithmetic operators : / and + for addition and division to find average.

---
+++ Exercise3.cpp
@@ -0,0 +1,25 @@
+//include statement
+#include<iostream>
+
+
+//using namespace statement
+using namespace std;
+
+
+int main() {
+//variable declaration
+int num1 = 10; // Change values as needed
+int num2 = 20;
+int num3 = 30;
+double average = (num1 + num2 + num3) / 3.0; //Calculate average
+
+//executable statements
+cout << "Value of num1: " << num1 << endl;
+cout << "Value of num2: " << num2 << endl;
+cout << "Value of num3: " << num3 << endl;
+cout << "Average: " << average << endl;
+
+
+//return statemtn
+return 0;
+}
