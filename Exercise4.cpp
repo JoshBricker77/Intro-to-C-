8b669a6d616a06b9fda5439d8b3112dedb291cbe
Joshua Bricker <joshbricker77@gmail.com>
2024-01-24 14:52:39 -0500

    Exercise 4 - Variable and Execution
    a basic program that uses include statement, namespace statement, variable declaration, and execution.

---
+++ Exercise4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variable declaration
+    int number = 10;
+
+    // Executable statements
+    cout << "The value of the variable 'number' is: " << number << endl;
+
+    // Return statement
+    return 0;
+}
