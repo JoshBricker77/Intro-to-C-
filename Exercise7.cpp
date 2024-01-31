6488318db9623e7c25b2deffa292609cb52151c6
Joshua Bricker <joshbricker77@gmail.com>
2024-01-31 03:34:35 -0500

    7th Exercise coding CPP
    Another print exercise for practice -- do not use spaces in file/folder names can cause programs not to run

---
+++ Exercise7.cpp
@@ -0,0 +1,10 @@
+//another print program
+//does it work if i comment before the preprocessor?
+//it is important to not use spaces in file names because it can cause your code not to run. i just learned this.
+#include<iostream>
+
+int main() {
+    std::cout << "i wrote this code without much help" << std::endl;
+    std::cout << "i almost have some simple coding understood" << std::endl;
+    return 0;
+}
