c0054f81ec40d5b49131396c4999430f047e67de
Joshua Bricker <joshbricker77@gmail.com>
2024-02-01 05:20:12 -0500

    another print program with some comments
    when writing this program i didnt format my int main correctly initially. i also typed int instead of #include for <iostream>

---
+++ exercise8.cpp
@@ -0,0 +1,8 @@
+//another attempt at writing a c++ print program from memory
+//i put std iostream instead of incldue -- remember in the preprocessor you need to choose your proper libraries
+//i also wrote out int() main instead of int main() remmeber ot write it properly.
+#include<iostream> 
+int main(){
+    std::cout << "Am i writing this code correctly?" << std::endl;
+    return 0;
+}
