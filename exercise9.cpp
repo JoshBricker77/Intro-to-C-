c372e556cf2082312cda089ccc5a9ed4d7f2f545
Joshua Bricker <joshbricker77@gmail.com>
2024-02-02 02:55:11 -0500

    SO CLOSE!
    i almost coded the entire program properly. the only thing i missed was the directions the arrows were facing i will do research on why they point the way they do to commit it to memory.

---
+++ exercise9.cpp
@@ -0,0 +1,7 @@
+//once i make a program that prints without using anything else i will move on to the next step
+#include<iostream>
+int main(){
+    std::cout << "i think i may have actually got this down finally im only unsure about the arrow directions." << std::endl;
+    return 0;
+}
+//this time i only messed up the arrow diretion... i will have to do some research on why the arrows need to point left
