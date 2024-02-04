b196d4078e765e172eb2f0415110dfcbd186103a
Joshua Bricker <joshbricker77@gmail.com>
2024-02-04 04:15:54 -0500

    sum of int
    A program that prints the sum of the variables x and y depends on what is assigned to the variables. this does not allow for user input.

---
+++ exercise11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int x = 2; // Define variable x and assign it the value 2
+    int y = 2; // Define variable y and assign it the value 2
+
+    int sum = x + y; // Add x and y and store the result in sum
+
+    // Print the result
+    std::cout << "The sum of x and y is: " << sum << std::endl;
+
+    return 0;
+}
