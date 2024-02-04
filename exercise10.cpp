7c8ed4e6c4f27d3d98e5a29eabe4c6d333ae876a
Joshua Bricker <joshbricker77@gmail.com>
2024-02-03 22:29:12 -0500

    first program no error
    finally made on with no errors. Follow programs will add some form of complexity.
    \

---
+++ exercise10.cpp
@@ -0,0 +1,6 @@
+//i think this program will be completley correct
+#include<iostream>
+int main() {
+    std::cout << "This will be my first properly coded c++ print statement with no help." << std::endl;
+    return 0;
+}
