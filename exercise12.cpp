9c8def67825faaaa207037eaf97dd3ddc3a28062
Joshua Bricker <joshbricker77@gmail.com>
2024-02-07 02:53:28 -0500

    Adding 2 to a Variable
    This program assigns a number to a variable and adds 2 to the integer then prints the results. the new terms used are int and the operator +

---
+++ exercise12.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main(){
+    int x = 5; // Value of 5 assigned to variable X
+
+    int result = x + 2; // utilize +2 to add 2 to the variable giving the answer 7.
+
+    std::cout << "The answer to " << x << " + 2 is " << result << std::endl;
+
+    return 0; 
+}
