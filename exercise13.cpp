ffc67af427225c27f33648fcdc7373bdd29b7efc
Joshua Bricker <joshbricker77@gmail.com>
2024-02-08 13:18:41 -0500

    +1
     Program that adds one to a variable

---
+++ exercise13.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int myVariable = 10; // Declare an integer variable and initialize it with a value of 10
+    
+    myVariable = myVariable + 1; // Add 1 to the variable
+    
+    // Output the result to the console
+    std::cout << "The value of myVariable after adding 1 is: " << myVariable << std::endl;
+    
+    return 0;
+}
