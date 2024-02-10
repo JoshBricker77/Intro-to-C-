178f00bc553064b956a9bfcebd58c3f3910e9151
Joshua Bricker <joshbricker77@gmail.com>
2024-02-09 21:34:12 -0500

    Multiply by 2

---
+++ ex14.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int X = 10; // Initialize X with a value
+    int result = X * 2; // Multiply X by 2
+
+    std::cout << "The result of " << X << " multiplied by 2 is: " << result << std::endl;
+
+    return 0;
+}
