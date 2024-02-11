5532c1f47d979100e949f8226321925384667e47
Joshua Bricker <joshbricker77@gmail.com>
2024-02-10 22:50:02 -0500

    Dividing Variable by 2
    assigned integer value to variable x(10) and divides it by 2 then prints a result with the answer

---
+++ ex15.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int x = 10; // Example value for x
+    x = x / 2; // Dividing x by 2
+    
+    std::cout << "The result of x divided by 2 is: " << x << std::endl;
+    
+    return 0;
+}
