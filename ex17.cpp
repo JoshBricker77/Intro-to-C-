b13eaf60b78c6a9a46c7a437c26052210fa76915
Joshua Bricker <joshbricker77@gmail.com>
2024-02-14 13:37:12 -0500

    dividing variable integer by 2
    This program declares the variable x = 10 then divides it by 2 and prints the result. my next programs will add in floating point numbers.

---
+++ ex17.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // Define the variable x
+    int x = 10;
+
+    // Divide x by 2 using integer division
+    int result = x / 2;
+
+    // Print the result
+    std::cout << "The result of " << x << " divided by 2 is " << result << std::endl;
+
+    return 0;
+}
